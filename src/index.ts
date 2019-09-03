4510be2435264d28d00581568b2d3d3f835d0082
jedmao <jedmao@outlook.com>
2019-09-03 17:56:34 -0500

    feat: wip

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,78 @@
-export default function foo() {
-	return 'bar'
+import * as cluster from 'cluster'
+import { readFileSync } from 'fs'
+import { cpus } from 'os'
+
+import chalk from 'chalk'
+import express = require('express')
+
+const numCPUs = cpus().length
+
+main()
+
+function main() {
+	if (cluster.isMaster) {
+		console.log(`Master ${process.pid} is running`)
+		console.log(`Forking ${numCPUs} workers`)
+		forkWorkers()
+	} else {
+		createServer()
+	}
+}
+
+function createServer() {
+	const app = express()
+
+	app.get('/', (_req, res) => {
+		res.status(200).end('hello world\n')
+	})
+
+	app.get('/download', (_req, res) => {
+		res.status(200).write('downloading...')
+		const contents = readFileSync(
+			__dirname + '/ArchLinuxARM-rpi-4-latest.tar.gz',
+		)
+		res.end(contents.toString())
+	})
+
+	app.get('/kill', (_req, res) => {
+		res.status(200).write(`Killing worker ${cluster.worker.process.pid}...`)
+		cluster.worker.kill()
+		res.end('done')
+	})
+
+	const server = app.listen(8000, () => {
+		console.log(chalk.green(`Worker ${process.pid} started`))
+	})
+
+	return server
+}
+
+function forkWorkers() {
+	for (let i = 0; i < numCPUs; i++) {
+		cluster.fork()
+	}
+
+	cluster.on('disconnect', worker => {
+		console.log(
+			(worker.exitedAfterDisconnect ? chalk.yellow : chalk.red)(
+				`Worker ${worker.process.pid} has ${
+					worker.exitedAfterDisconnect ? 'gracefully' : 'unexpectedly'
+				} disconnected`,
+			),
+		)
+	})
+
+	cluster.on('exit', (worker, code, signal) => {
+		if (worker.exitedAfterDisconnect) {
+			console.log(chalk.yellow(`Worker ${worker.process.pid} restarting...`))
+		} else {
+			console.log(
+				chalk.red('Worker %d died (%s). Restarting...'),
+				worker.process.pid,
+				signal || code,
+			)
+		}
+
+		cluster.fork()
+	})
 }
